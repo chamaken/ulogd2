ea5287626a52887071a44985ea6a1f81d0ee272a
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2014-04-02 14:50:41 +0900

    sprint: add copyright

--- output/sprint/ulogd_output_SPRINT-parser.y
+++ output/sprint/ulogd_output_SPRINT-parser.y
@@ -1,4 +1,9 @@
-/*
+/* ulogd_output_SPRINT-parser.y
+ *
+ * ulogd output target for sending value specified `form' in config.
+ *
+ * (C) 2014 by Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2
  *  as published by the Free Software Foundation.
--- output/sprint/ulogd_output_SPRINT.h
+++ output/sprint/ulogd_output_SPRINT.h
@@ -1,6 +1,26 @@
 #ifndef _SPRINT_H
 #define _SPRINT_H
 
+/* ulogd_output_SPRINT.h
+ *
+ * ulogd output target for sending value specified `form' in config.
+ *
+ * (C) 2014 by Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include "ulogd_output_SPRINT-parser.h"
 
 enum sprint_node_type {
