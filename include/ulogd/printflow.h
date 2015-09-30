2ecc8185b79fc9fd8145dafe471f59abde9cc1b4
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2015-09-30 10:39:43 +0900

    plugins: delete MY unused modules

--- include/ulogd/printflow.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PRINTFLOW_H
-#define _PRINTFLOW_H
-
-#define FLOW_IDS 17
-extern struct ulogd_key printflow_keys[FLOW_IDS];
-
-int printflow_print(struct ulogd_key *res, char *buf);
-
-#endif
